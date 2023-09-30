c013539f5010206fd5cc04b021bd08a9c0da26d3
BANZOM <aditya.panwar.2208@gmail.com>
2023-09-30 09:22:23 +0530

    Worked with @Service @Value @PropertySource @Bean- PSPC

--- src/main/java/com/firstspringboot/firstspringboot/FirstspringbootApplication.java
+++ src/main/java/com/firstspringboot/firstspringboot/FirstspringbootApplication.java
@@ -3,6 +3,7 @@ package com.firstspringboot.firstspringboot;
 import org.springframework.boot.*;
 import org.springframework.boot.autoconfigure.*;
 import org.springframework.context.ConfigurableApplicationContext;
+import com.firstspringboot.firstspringboot.service.DatabaseService;
 
 import com.firstspringboot.firstspringboot.resourcehandson.ResourcesInjectMain;
 import com.firstspringboot.firstspringboot.service.GetFactorial;
@@ -11,16 +12,22 @@ import com.firstspringboot.firstspringboot.service.GetFactorial;
 public class FirstspringbootApplication {
 
 	public static void main(String[] args) {
-		ConfigurableApplicationContext context =SpringApplication.run(FirstspringbootApplication.class, args);
+		ConfigurableApplicationContext context = SpringApplication.run(FirstspringbootApplication.class, args);
 
-		PojoBean pojoBean = context.getBean(PojoBean.class);
-		pojoBean.show();
+		// PojoBean pojoBean = context.getBean(PojoBean.class);
+		// pojoBean.show();
 
-		ResourcesInjectMain resourcesInjectMain = context.getBean(ResourcesInjectMain.class);
-		System.out.println(resourcesInjectMain);
+		// ResourcesInjectMain resourcesInjectMain =
+		// context.getBean(ResourcesInjectMain.class);
+		// System.out.println(resourcesInjectMain);
 
-		GetFactorial getFactorial = context.getBean(GetFactorial.class);
-		System.out.println("Getting Factorial of 5 using Service bean: " + getFactorial.find(5));
+		// GetFactorial getFactorial = context.getBean(GetFactorial.class);
+		// System.out.println("Getting Factorial of 5 using Service bean: " +
+		// getFactorial.find(5));
+
+		DatabaseService databaseService = context.getBean(DatabaseService.class);
+		System.out.println(databaseService);
+
 	}
 
 }
---
+++ src/main/java/configs/DatabaseConfig.java
@@ -0,0 +1,16 @@
+package configs;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
+
+@Configuration
+@PropertySource("src/main/resources:application.properties")
+public class DatabaseConfig {
+
+    @Bean
+    public static PropertySourcesPlaceholderConfigurer propertySourcesPlaceholderConfigurer() {
+        return new PropertySourcesPlaceholderConfigurer();
+    }
+}

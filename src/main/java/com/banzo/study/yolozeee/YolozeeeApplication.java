f4b781e9b89630013c2b8010dc1d62d8f91783a0
banzom <banzo.aditya@gmail.com>
2024-05-02 22:30:17 +0530

    Add initial project files and configurations

---
+++ src/main/java/com/banzo/study/yolozeee/YolozeeeApplication.java
@@ -0,0 +1,13 @@
+package com.banzo.study.yolozeee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YolozeeeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YolozeeeApplication.class, args);
+	}
+
+}

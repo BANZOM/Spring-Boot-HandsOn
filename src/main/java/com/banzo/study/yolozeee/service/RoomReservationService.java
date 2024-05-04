ae1e53b5179b066966eaf9a56edc7ebf07a4f952
banzom <aditya.panwar.2208@gmail.com>
2024-05-04 14:29:50 +0530

    Refactor RoomReservationService to populate roomReservations with reservation data

--- src/main/java/com/banzo/study/yolozeee/service/RoomReservationService.java
+++ src/main/java/com/banzo/study/yolozeee/service/RoomReservationService.java
@@ -1,10 +1,16 @@
 package com.banzo.study.yolozeee.service;
 
 import java.sql.Date;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.Optional;
 
 import org.springframework.stereotype.Service;
 
+import com.banzo.study.yolozeee.data.entity.Guest;
+import com.banzo.study.yolozeee.data.entity.Reservation;
+import com.banzo.study.yolozeee.data.entity.Room;
 import com.banzo.study.yolozeee.data.repository.GuestRepository;
 import com.banzo.study.yolozeee.data.repository.ReservationRepository;
 import com.banzo.study.yolozeee.data.repository.RoomRepository;
@@ -34,5 +40,31 @@ public class RoomReservationService {
         } else {
             date = new Date(new java.util.Date().getTime());
         }
+
+        Map<Long, RoomReservation> roomReservations = new HashMap<>();
+
+        List<Room> rooms = this.roomRepository.findAll();
+        rooms.forEach(room -> {
+            RoomReservation roomReservation = new RoomReservation();
+            roomReservation.setRoomId(room.getId());
+            roomReservation.setRoomName(room.getName());
+            roomReservation.setRoomNumber(room.getRoomNumber());
+            roomReservations.put(roomReservation.getRoomId(), roomReservation);
+        });
+
+        List<Reservation> reservations = this.reservationRepository.findByReservationDate(date);
+        reservations.forEach(reservation -> {
+            RoomReservation roomReservation = roomReservations.get(reservation.getRoomId());
+            roomReservation.setReservationId(reservation.getId());
+            roomReservation.setReservationDate(reservation.getReservationDate().toString());
+            Optional<Guest> guest = this.guestRepository.findById(reservation.getGuestId());
+            if(guest.isPresent()) {
+                roomReservation.setGuestId(guest.get().getId());
+                roomReservation.setFirstName(guest.get().getFirstName());
+                roomReservation.setLastName(guest.get().getLastName());
+            }      
+        });
+
+        return roomReservations.values().stream().toList();
     }
 }

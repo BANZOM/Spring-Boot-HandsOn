5f9736d4f6d581001b2c8a7081be03bd2dffccfc
banzom <aditya.panwar.2208@gmail.com>
2024-05-08 19:55:57 +0530

    Add POST method to create a new room in RoomApiController

--- src/main/java/com/banzo/study/yolozeee/web/api/RoomApiController.java
+++ src/main/java/com/banzo/study/yolozeee/web/api/RoomApiController.java
@@ -2,8 +2,12 @@ package com.banzo.study.yolozeee.web.api;
 
 import java.util.List;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.banzo.study.yolozeee.data.entity.Room;
@@ -21,6 +25,12 @@ public class RoomApiController {
     @GetMapping
     public List<Room> getRooms() {
         return this.roomRepository.findAll();
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public Room createRoom(@RequestBody Room room) {
+        return this.roomRepository.save(room);
     }
 
 }

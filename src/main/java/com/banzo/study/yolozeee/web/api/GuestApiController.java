a227a8cf6f986b140a6eb88f40e7eb309209db64
banzom <aditya.panwar.2208@gmail.com>
2024-05-08 20:39:35 +0530

    Refactor GuestApiController to handle CRUD operations for guests

--- src/main/java/com/banzo/study/yolozeee/web/api/GuestApiController.java
+++ src/main/java/com/banzo/study/yolozeee/web/api/GuestApiController.java
@@ -2,12 +2,16 @@ package com.banzo.study.yolozeee.web.api;
 
 import java.util.List;
 
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.banzo.study.yolozeee.data.entity.Guest;
 import com.banzo.study.yolozeee.data.repository.GuestRepository;
+import com.banzo.study.yolozeee.web.exception.BadRequestException;
 import com.banzo.study.yolozeee.web.exception.NotFoundException;
 
 @RestController
@@ -28,6 +32,22 @@ public class GuestApiController {
     @GetMapping("/{id}")
     public Guest getGuest(long id) {
         return this.guestRepository.findById(id).orElseThrow(() -> new NotFoundException("Guest not found with id: " + id));
+    }
+
+    @PostMapping
+    public Guest createGuest(Guest guest) {
+        return this.guestRepository.save(guest);
+    }
+
+    @PutMapping("/{id}")
+    public Guest updateGuest(long id, Guest guest) {
+        this.guestRepository.findById(id).orElseThrow(() -> new BadRequestException("Guest not found with id: " + id));
+        return this.guestRepository.save(guest);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteGuest(long id) {
+        this.guestRepository.deleteById(id);
     }
 
 }

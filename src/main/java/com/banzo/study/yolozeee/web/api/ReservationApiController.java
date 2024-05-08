16f8a8d5ff6982b623081585992c59cda2358502
banzom <aditya.panwar.2208@gmail.com>
2024-05-08 21:13:06 +0530

    Refactor ReservationApiController to handle date filtering in getReservations method

--- src/main/java/com/banzo/study/yolozeee/web/api/ReservationApiController.java
+++ src/main/java/com/banzo/study/yolozeee/web/api/ReservationApiController.java
@@ -1,14 +1,18 @@
 package com.banzo.study.yolozeee.web.api;
 
 
+import java.sql.Date;
 import java.util.List;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.banzo.study.yolozeee.data.entity.Reservation;
 import com.banzo.study.yolozeee.data.repository.ReservationRepository;
+
+import io.micrometer.common.util.StringUtils;
 
 @RestController
 @RequestMapping("/api/reservations")
@@ -21,7 +25,11 @@ public class ReservationApiController {
     }
 
     @GetMapping
-    public List<Reservation> getReservations() {
+    public List<Reservation> getReservations(@RequestParam(value = "date", required = false) String datesString) {
+        if (StringUtils.isNotBlank(datesString)) {
+            Date date = Date.valueOf(datesString);
+            return this.reservationRepository.findByReservationDate(date);
+        }
         return this.reservationRepository.findAll();
     }
 

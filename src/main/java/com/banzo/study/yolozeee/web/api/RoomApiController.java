384455b14582b5079231ca1e2dfe325824b52077
banzom <aditya.panwar.2208@gmail.com>
2024-05-08 20:11:29 +0530

    Add PUT method to update a room in RoomApiController

--- src/main/java/com/banzo/study/yolozeee/web/api/RoomApiController.java
+++ src/main/java/com/banzo/study/yolozeee/web/api/RoomApiController.java
@@ -6,6 +6,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -13,6 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.banzo.study.yolozeee.data.entity.Room;
 import com.banzo.study.yolozeee.data.repository.RoomRepository;
+import com.banzo.study.yolozeee.web.exception.BadRequestException;
 import com.banzo.study.yolozeee.web.exception.NotFoundException;
 
 @RestController
@@ -38,6 +40,12 @@ public class RoomApiController {
     @GetMapping("/{id}")
     public Room getRoom(@PathVariable("id") long id) {
         return this.roomRepository.findById(id).orElseThrow(() -> new NotFoundException("Room not found with id: " + id));
+    }
+
+    @PutMapping("/{id}")
+    public Room updateRoom(@PathVariable("id") long id, @RequestBody Room room) {
+        this.roomRepository.findById(id).orElseThrow(() -> new BadRequestException("Room not found with id: " + id));
+        return this.roomRepository.save(room);
     }
 
 }

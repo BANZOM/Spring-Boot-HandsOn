2b943d38e7158c08c71fa9d32cfae38dfc05b258
banzom <aditya.panwar.2208@gmail.com>
2024-06-06 17:36:00 +0530

    Add TourPackageRepository and TourRepository interfaces

---
+++ src/main/java/com/banzo/study/yolozeee/repository/TourPackageRepository.java
@@ -0,0 +1,9 @@
+package com.banzo.study.yolozeee.repository;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.banzo.study.yolozeee.domain.TourPackage;
+
+public interface TourPackageRepository extends CrudRepository<TourPackage, String>{
+
+}
---
+++ src/main/java/com/banzo/study/yolozeee/repository/TourRepository.java
@@ -0,0 +1,9 @@
+ package com.banzo.study.yolozeee.repository;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.banzo.study.yolozeee.domain.Tour;
+
+public interface TourRepository extends CrudRepository<Tour, Integer>{
+
+}

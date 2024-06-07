e07ecc1d447820e14aab1b10ba6a93a06b8c85e8
banzom <aditya.panwar.2208@gmail.com>
2024-06-07 09:23:03 +0530

    Add findByTourPackageCode method to TourRepository

--- src/main/java/com/banzo/study/yolozeee/repository/TourRepository.java
+++ src/main/java/com/banzo/study/yolozeee/repository/TourRepository.java
@@ -1,9 +1,11 @@
  package com.banzo.study.yolozeee.repository;
 
+import java.util.List;
+
 import org.springframework.data.repository.CrudRepository;
 
 import com.banzo.study.yolozeee.domain.Tour;
 
 public interface TourRepository extends CrudRepository<Tour, Integer>{
-
+    List<Tour> findByTourPackageCode(String code);
 }

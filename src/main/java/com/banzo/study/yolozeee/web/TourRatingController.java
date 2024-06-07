be396d52956e0fccddbb0f14020ee578075f8870
banzom <aditya.panwar.2208@gmail.com>
2024-06-07 20:03:48 +0530

    feat: Add endpoint for creating tour ratings

--- src/main/java/com/banzo/study/yolozeee/web/TourRatingController.java
+++ src/main/java/com/banzo/study/yolozeee/web/TourRatingController.java
@@ -1,25 +1,56 @@
 package com.banzo.study.yolozeee.web;
 
+import java.util.NoSuchElementException;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.banzo.study.yolozeee.domain.Tour;
+import com.banzo.study.yolozeee.domain.TourRating;
+import com.banzo.study.yolozeee.domain.TourRatingPk;
 import com.banzo.study.yolozeee.repository.TourRatingRepository;
 import com.banzo.study.yolozeee.repository.TourRepository;
 
 
 @RestController
-@RequestMapping("/tour/{tourId}/ratings")
+@RequestMapping("/tours/{tourId}/ratings")
 public class TourRatingController {
 
     TourRatingRepository tourRatingRepository;
     TourRepository tourRepository;
 
+    @Autowired
     public TourRatingController(TourRatingRepository tourRatingRepository, TourRepository tourRepository) {
         this.tourRatingRepository = tourRatingRepository;
         this.tourRepository = tourRepository;
     }
 
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public void createTourRating(@PathVariable(value = "tourId") Integer tourId,@RequestBody @Validated RatingDto ratingDto) {
+        Tour tour = verifyTour(tourId);
+        tourRatingRepository.save(new TourRating(new TourRatingPk(tour, ratingDto.getCustomerId()), ratingDto.getScore(), ratingDto.getComment()));        
+    }
+
     protected TourRatingController() {
+    }
+
+    private Tour verifyTour(Integer tourId) {
+        return tourRepository.findById(tourId).orElseThrow(() -> new NoSuchElementException("Tour does not exist " + tourId));
+    }
+
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    @ExceptionHandler(NoSuchElementException.class)
+    public String return400(NoSuchElementException ex) {
+        return ex.getMessage();
     }
 
 }

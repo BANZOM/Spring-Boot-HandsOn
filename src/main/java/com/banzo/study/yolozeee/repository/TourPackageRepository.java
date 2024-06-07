ae320b955b54f592832474c7130eb75d1ac45438
banzom <aditya.panwar.2208@gmail.com>
2024-06-07 10:19:14 +0530

    Add delete methods to TourPackageRepository

--- src/main/java/com/banzo/study/yolozeee/repository/TourPackageRepository.java
+++ src/main/java/com/banzo/study/yolozeee/repository/TourPackageRepository.java
@@ -3,10 +3,42 @@ package com.banzo.study.yolozeee.repository;
 import java.util.Optional;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.data.rest.core.annotation.RestResource;
 
 import com.banzo.study.yolozeee.domain.TourPackage;
 
+@RepositoryRestResource(collectionResourceRel = "packages", path = "packages")
 public interface TourPackageRepository extends CrudRepository<TourPackage, String>{
 
     Optional<TourPackage> findByName(String name);
+
+    @Override
+    @RestResource(exported = false) // to disable the method and shows 405 Method Not Allowed
+    void delete(TourPackage entity);
+
+    @Override
+    @RestResource(exported = false)
+    void deleteAll();
+
+    @Override
+    @RestResource(exported = false)
+    void deleteAll(Iterable<? extends TourPackage> entities);
+
+    @Override
+    @RestResource(exported = false)
+    void deleteAllById(Iterable<? extends String> ids);
+
+    @Override
+    void deleteById(String id);
+
+    @Override
+    @RestResource(exported = false)
+    <S extends TourPackage> S save(S entity);
+
+    @Override
+    @RestResource(exported = false)
+    <S extends TourPackage> Iterable<S> saveAll(Iterable<S> entities);
+
+    
 }
--- src/main/java/com/banzo/study/yolozeee/repository/TourRepository.java
+++ src/main/java/com/banzo/study/yolozeee/repository/TourRepository.java
@@ -1,11 +1,15 @@
  package com.banzo.study.yolozeee.repository;
 
-import java.util.List;
-
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 
 import com.banzo.study.yolozeee.domain.Tour;
 
 public interface TourRepository extends CrudRepository<Tour, Integer>{
-    List<Tour> findByTourPackageCode(String code);
+    // PagingAndSortingRepository is a sub-interface of CrudRepository that provides additional methods to retrieve entities using pagination and sorting.
+    // includes additional methods to retrieve entities using pagination and sorting like page, size, sort, desc, asc, etc.
+
+    Page<Tour> findByTourPackageCode(@Param("code") String code, Pageable pageable);
 }
